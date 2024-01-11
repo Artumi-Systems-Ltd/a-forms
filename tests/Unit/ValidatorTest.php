590adac832e395cf2e5091dae7a192453bd615ed
Richard Fletcher <richard@artumi.com>
2024-01-11 13:12:23 +0000

    chore: separate forms

--- tests/Unit/ValidatorTest.php
+++ tests/Unit/ValidatorTest.php
@@ -1,24 +1,8 @@
 <?php
 declare(strict_types=1);
 use Tests\TestCase;
-use Artumi\Forms\Form;
 use Artumi\Forms\Widget\TextArea;
-
-class ValidateTextAreaForm extends Form
-{
-    public function __construct(public string $id)
-    {
-        $desc = new TextArea('description','Description');
-        $this->addWidget($desc);
-        $this->addButton('ok','Submit');
-    }
-    public function validators() : array
-    {
-        return [
-            'description'=>'required'
-        ];
-    }
-}
+use Tests\Forms\ValidateTextAreaForm;
 
 class ValidatorTest extends TestCase
 {
