ec37b87b15275bf28badab8f0aec916ba5288d6e
Richard Fletcher <richard@artumi.com>
2024-04-18 09:50:04 +0100

    imrpov(text): Attributes can be added

--- src/Widget/Text.php
+++ src/Widget/Text.php
@@ -5,6 +5,23 @@ use Artumi\Forms\Widget;
 
 class Text extends Widget{
 
+    public $allowed=[
+        'list',
+        'pattern',
+        'size',
+        'autocorrect',
+        'type',
+        'disabled',
+        'maxlength',
+        'minlength',
+        'placeholder',
+        'readonly',
+        'required',
+        'spellcheck',
+        'wrap',
+        'class',
+        'style',
+    ];
     public function html() : string {
         return $this->label()
             .'<input '.$this->attribString().'type="text" value="'.htmlspecialchars($this->get(), ENT_QUOTES).'"/>'
