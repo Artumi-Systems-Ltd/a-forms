2d42685dc91d828528f370080b639a009b8f6212
Richard Fletcher <richard@artumi.com>
2024-06-07 20:11:00 +0100

    whitespace

--- src/Form.php
+++ src/Form.php
@@ -1,60 +1,63 @@
 <?php
+
 declare(strict_types=1);
+
 namespace ArtumiSystemsLtd\AForms;
 
 use Illuminate\Support\Facades\Validator as FValidator;
 use Illuminate\Validation\Validator;
 use Illuminate\Http\Request;
-use ArtumiSystemsLtd\AForms\Trait\Attributes;
+use \ArtumiSystemsLtd\AForms\Trait\Attributes;
 use InvalidArgumentException;
 
-abstract class Form {
+abstract class Form
+{
     use Attributes;
 
-    private string $defaultValidationMsg='There has been an error, please see details below:';
-    private string $sValidationMsg='';
-    private string $buttonPressed='';
-    private array  $widgets=[];
-    private array  $buttons=[];
-    private ?Validator $lastValidator=null;
-    private string $sAction='';
-    private string $sMethod='post';
+    private string $defaultValidationMsg = 'There has been an error, please see details below:';
+    private string $sValidationMsg = '';
+    private string $buttonPressed = '';
+    private array  $widgets = [];
+    private array  $buttons = [];
+    private ?Validator $lastValidator = null;
+    private string $sAction = '';
+    private string $sMethod = 'post';
 
 
-    public $allowed=[
+    public $allowed = [
         'class',
         'style',
     ];
 
-    public function __construct(public string $id,
-        public array $initialAttribs){
-        $this->attribs=$this->initialAttribs;
-
+    public function __construct(
+        public string $id,
+        public array $initialAttribs
+    ) {
+        $this->attribs = $this->initialAttribs;
     }
 
-    public function id() : string {
+    public function id(): string
+    {
         return $this->id;
     }
 
-    public function addButton(string $name, string $value) : void
+    public function addButton(string $name, string $value): void
     {
-        $this->buttons[$name]=new Button($name, $value);
+        $this->buttons[$name] = new Button($name, $value);
     }
     public function addWidget(Widget $widget)
     {
         $widget->setForm($this);
-        $this->widgets[$widget->name]=$widget;
+        $this->widgets[$widget->name] = $widget;
     }
-    public function removeWidget(Widget|string $widget) : bool {
-        if(is_object($widget))
-        {
-            $name=$widget->name;
+    public function removeWidget(Widget|string $widget): bool
+    {
+        if (is_object($widget)) {
+            $name = $widget->name;
+        } else {
+            $name = $widget;
         }
-        else {
-            $name=$widget;
-        }
-        if($this->hasWidget($name))
-        {
+        if ($this->hasWidget($name)) {
             unset($this->widgets[$name]);
             return true;
         }
@@ -64,240 +67,233 @@ abstract class Form {
     {
         return isset($this->widgets[$name]);
     }
-    public function getWidget(string $name) : Widget {
+    public function getWidget(string $name): Widget
+    {
         return $this->widgets[$name];
     }
 
-    public function html() : string
+    public function html(): string
     {
         $this->updateValidation();
-        $s='<form id="'.htmlspecialchars($this->id,ENT_QUOTES).'"';
+        $s = '<form id="' . htmlspecialchars($this->id, ENT_QUOTES) . '"';
         $attribs = $this->attribString(false);
-        if($attribs)
-        $s.=' '.$attribs;
-        if($this->sAction)
-        {
-            $s.=' action="'.htmlspecialchars($this->sAction, ENT_QUOTES).'"';
+        if ($attribs)
+            $s .= ' ' . $attribs;
+        if ($this->sAction) {
+            $s .= ' action="' . htmlspecialchars($this->sAction, ENT_QUOTES) . '"';
         }
-        if($this->sMethod)
-        {
-            switch($this->sMethod)
-            {
+        if ($this->sMethod) {
+            switch ($this->sMethod) {
                 case 'get':
                 case 'post':
                 case 'dialog':
-                    $s.=' method="'.$this->sMethod.'"';
+                    $s .= ' method="' . $this->sMethod . '"';
                     break;
                 case 'put':
                 case 'patch':
                 case 'delete':
-                    $s.=' method="post"';
+                    $s .= ' method="post"';
                     break;
             }
         }
-        $s.='>';
-        $s.=$this->formMethodSpoof();
-        $s.=$this->csrf();
-        $s.=$this->formValMsgHTML();
-        foreach($this->widgets as $widget)
-        {
-            $s.=$widget->html();
+        $s .= '>';
+        $s .= $this->formMethodSpoof();
+        $s .= $this->csrf();
+        $s .= $this->formValMsgHTML();
+        foreach ($this->widgets as $widget) {
+            $s .= $widget->html();
         }
-        foreach($this->buttons as $button)
-        {
-            $s.=$button->html();
+        foreach ($this->buttons as $button) {
+            $s .= $button->html();
         }
-        $s.='</form>';
+        $s .= '</form>';
         return $s;
     }
-    public function formMethodSpoof() : string {
-        switch($this->sMethod)
-        {
+    public function formMethodSpoof(): string
+    {
+        switch ($this->sMethod) {
             case 'put':
             case 'delete':
             case 'patch':
-            return '<input type="hidden" name="_method" value="'. $this->sMethod.'">';
+                return '<input type="hidden" name="_method" value="' . $this->sMethod . '">';
         }
         return '';
     }
-    public function __get(string $sName) : Widget {
+    public function __get(string $sName): Widget
+    {
         return $this->widgets[$sName];
     }
-    public function __set(string $sName, $value) : void {
+    public function __set(string $sName, $value): void
+    {
         $this->widgets[$sName]->set($value);
     }
-    public function initialPopulate(array $a): void {
+    public function initialPopulate(array $a): void
+    {
 
-        foreach ($a as $k=>$v)
-        {
-            if(isset($this->widgets[$k]))
+        foreach ($a as $k => $v) {
+            if (isset($this->widgets[$k]))
                 $this->widgets[$k]->initialPopulate($a);
             //no buttons can be pressed on an initial populate
         }
     }
-    public function populateFromArray(array $a) : void {
-        foreach ($a as $k=>$v)
-        {
-            if(isset($this->widgets[$k]))
+    public function populateFromArray(array $a): void
+    {
+        foreach ($a as $k => $v) {
+            if (isset($this->widgets[$k]))
                 $this->widgets[$k]->populateFromArray($a);
             else if (isset($this->buttons[$k]))
-                $this->buttonPressed=$k;
+                $this->buttonPressed = $k;
         }
     }
-    public function buttonPressed() : string
+    public function buttonPressed(): string
     {
         return $this->buttonPressed;
     }
-    public function pack() : array {
-        $a=[];
-        foreach($this->widgets as $name=>$widget)
-        {
-            $widgetVals= $widget->pack();
-            foreach($widgetVals as $widgetName=>$widgetVal)
-            {
-                $a[$widgetName]=$widgetVal;
+    public function pack(): array
+    {
+        $a = [];
+        foreach ($this->widgets as $name => $widget) {
+            $widgetVals = $widget->pack();
+            foreach ($widgetVals as $widgetName => $widgetVal) {
+                $a[$widgetName] = $widgetVal;
             }
         }
-        if($this->buttonPressed)
-        {
-            $a[$this->buttonPressed]='';
+        if ($this->buttonPressed) {
+            $a[$this->buttonPressed] = '';
         }
         return $a;
     }
-    public function packChanged() : array {
+    public function packChanged(): array
+    {
 
-        $a=[];
-        foreach($this->widgets as $name=>$widget)
-        {
-            if($widget->changed())
-            {
-                $widgetVals= $widget->pack();
-                foreach($widgetVals as $widgetName=>$widgetVal)
-                {
-                    $a[$widgetName]=$widgetVal;
+        $a = [];
+        foreach ($this->widgets as $name => $widget) {
+            if ($widget->changed()) {
+                $widgetVals = $widget->pack();
+                foreach ($widgetVals as $widgetName => $widgetVal) {
+                    $a[$widgetName] = $widgetVal;
                 }
             }
         }
         return $a;
     }
-    public function validators() : array
+    public function validators(): array
     {
-        $a=[];
-        foreach($this->widgets as $name=>$widget)
-        {
+        $a = [];
+        foreach ($this->widgets as $name => $widget) {
             $validator = $widget->validator();
-            if($validator)
-            {
-                $a[$name]=$validator;
+            if ($validator) {
+                $a[$name] = $validator;
             }
         }
         return $a;
-
     }
-    public function validate() : bool {
+    public function validate(): bool
+    {
         $validator = FValidator::make($this->pack(), $this->validators());
-        $this->lastValidator=$validator;
+        $this->lastValidator = $validator;
         return $validator->passes();
     }
-    public function lastValidator(){
+    public function lastValidator()
+    {
         return $this->lastValidator;
     }
     /** Passes validation information collected during a validate()
-    *   call to widgets
-    **/
+     *   call to widgets
+     **/
     private function updateValidation()
     {
-        if($this->lastValidator)
-        {
-            $bGotOne=false;
-            foreach($this->widgets as $name=>$widget)
-            {
+        if ($this->lastValidator) {
+            $bGotOne = false;
+            foreach ($this->widgets as $name => $widget) {
                 $errors = $this->lastValidator->errors()->get($name);
-                if(count($errors))
-                {
-                    $bGotOne=true;
+                if (count($errors)) {
+                    $bGotOne = true;
                     $widget->setValidation($errors);
                 }
-                if($bGotOne)
-                {
+                if ($bGotOne) {
                     $this->setValidationMsg($this->defaultValidationMsg);
                 }
             }
-        }
-        else {
+        } else {
             $this->resetValidation();
         }
     }
-    private function resetValidation() : void {
-        $this->sValidationMsg='';
-        foreach($this->widgets as $widget){
+    private function resetValidation(): void
+    {
+        $this->sValidationMsg = '';
+        foreach ($this->widgets as $widget) {
             $widget->resetValidation();
         }
     }
-    public function setValidationMsg(string $sValMsg): void {
-        $this->sValidationMsg=$sValMsg;
+    public function setValidationMsg(string $sValMsg): void
+    {
+        $this->sValidationMsg = $sValMsg;
     }
-    private function formValMsgHTML():string {
-        if(!$this->sValidationMsg)
-        {
+    private function formValMsgHTML(): string
+    {
+        if (!$this->sValidationMsg) {
             return '';
         }
-        return '<div id="'.$this->id().'_valmsg" class="validation">'.$this->sValidationMsg.'</div>';
+        return '<div id="' . $this->id() . '_valmsg" class="validation">' . $this->sValidationMsg . '</div>';
     }
-    public function setAction(string $url) : void {
-        $this->sAction=$url;
+    public function setAction(string $url): void
+    {
+        $this->sAction = $url;
     }
-    public function setMethod(string $method) : void {
-        switch($method) {
+    public function setMethod(string $method): void
+    {
+        switch ($method) {
             case 'get':
             case 'post':
             case 'put':
             case 'delete':
             case 'dialog':
             case 'patch':
-                $this->sMethod=$method;
+                $this->sMethod = $method;
                 break;
             default:
-            throw new InvalidArgumentException('setMethod only accespts get|post|put|patch|dialog, but received '.$method);
+                throw new InvalidArgumentException('setMethod only accespts get|post|put|patch|dialog, but received ' . $method);
         }
     }
-    public function populateFromRequest(Request $request) : void {
-        $aVals=$request->all();
+    public function populateFromRequest(Request $request): void
+    {
+        $aVals = $request->all();
         $this->populateFromTransaction($aVals);
     }
     public function populateFromTransaction($aVals)
     {
-        foreach ($this->widgets as $name=>$widget)
-        {
+        foreach ($this->widgets as $name => $widget) {
             $widget->populateFromTransaction($aVals);
         }
-        foreach($this->buttons as $button)
-        {
-            if(isset($aVals[$button->name]))
-                $this->buttonPressed=$button->name;
+        foreach ($this->buttons as $button) {
+            if (isset($aVals[$button->name]))
+                $this->buttonPressed = $button->name;
         }
     }
-    public function csrf() : string {
+    public function csrf(): string
+    {
         $t = csrf_token();
-        if($t) {
-            return '<input type="hidden" name="_token" value="'.htmlspecialchars($t, ENT_QUOTES).'">';
+        if ($t) {
+            return '<input type="hidden" name="_token" value="' . htmlspecialchars($t, ENT_QUOTES) . '">';
         }
         return '';
     }
     /**
-    * Saves the current data to the session
-    */
-    public function flash() : void {
-        session()->flash('form-'.$this->id(), $this->pack());
+     * Saves the current data to the session
+     */
+    public function flash(): void
+    {
+        session()->flash('form-' . $this->id(), $this->pack());
     }
     /*
     * Loads the data from the session flash, if there is any.
     * @return true if something was loaded.
     **/
-    public function loadFromFlash(): bool {
-        $data = session('form-'.$this->id());
-        if($data)
-        {
+    public function loadFromFlash(): bool
+    {
+        $data = session('form-' . $this->id());
+        if ($data) {
             $this->populateFromArray($data);
             return true;
         }
